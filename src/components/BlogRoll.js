ae752f9d7a17eabc9c539cb1de381213793d2b9d
Rodrigo <54219888+roolgo@users.noreply.github.com>
2022-06-01 16:50:54 -0300

    Update BlogRoll.js

--- src/components/BlogRoll.js
+++ src/components/BlogRoll.js
@@ -66,9 +66,9 @@ class BlogRoll extends React.Component {
             </div>
           ))}
       </div>
-       <div className="card">
-       <p>AD GOES HERE</p>
-       </div>
+//        <div className="card">
+//        <p>AD GOES HERE</p>
+//        </div>
     </>)
   }
 }
